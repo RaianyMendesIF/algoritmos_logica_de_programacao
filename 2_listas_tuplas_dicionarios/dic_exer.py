a93ce1d9c9b3789bd1adbf37598aafa8673eff6f
RaianyMendes <raianyvitoriapm@gmail.com>
2025-05-24 19:07:49 -0300

    descomentando exercicios

--- 2_listas_tuplas_dicionarios/dic_exer.py
+++ 2_listas_tuplas_dicionarios/dic_exer.py
@@ -2,37 +2,37 @@
 Ex1 - Leia o nome de 5 pessoas e a armazene em um dicionario
 '''
 
-# dic = {}
-# for i in range(5):
-#     print("Usuário", i+1)
-#     cpf = input(f"Digite seu cpf: ")
-#     nome = input(f"Digite o nome: ") 
-#     dic[cpf] =  nome
-# print(dic)
+dic = {}
+for i in range(5):
+    print("Usuário", i+1)
+    cpf = input(f"Digite seu cpf: ")
+    nome = input(f"Digite o nome: ") 
+    dic[cpf] =  nome
+print(dic)
 
 
 '''
 Ex2 - Crie um programa para gerar um dicionário com 20 números inteiros, mostre a soma de todos os elementos
 '''
 
-# from random import randint
-# num = {}
+from random import randint
+num = {}
 
-# for i in range(20):
-#     num[i] = randint(1, 10)
+for i in range(20):
+    num[i] = randint(1, 10)
     
-# soma = sum(num.values())
+soma = sum(num.values())
 
-# print(num)
-# print(soma)
+print(num)
+print(soma)
 
 # pass ou ... - por enquanto não há nada mas vai haver
 
 # Compreensão de lista
 
-# lista = [x for x in range(3) if x < 2]
+lista = [x for x in range(3) if x < 2]
 
-# print(lista)
+print(lista)
 
 
 
